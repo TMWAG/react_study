1e17641c43e5ee078dd740b25a7f4c749f5e759c
TMWAG <semyon.valyaev@gmail.com>
2023-05-11 18:42:34 +0300

    компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://avatars.mds.yandex.net/i?id=e6502707d86cc8914e8d81f7e66db73f4b4a2ef8-4952143-images-thumbs&n=13" alt="logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+      <img src="https://www.borealcm.com/wp-content/uploads/2015/06/placeholderimg-1024x576.png" alt="pfp" />
+      <div>ava + desc</div>
+      <div>
+        My posts
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
