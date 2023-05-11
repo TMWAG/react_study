45d4f7438c9290060ba1fbc542b3b129804a349f
TMWAG <semyon.valyaev@gmail.com>
2023-05-11 18:08:14 +0300

    новый макет

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-import Technologies from './Technologies';
-import Header from './Header';
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <Technologies/>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://avatars.mds.yandex.net/i?id=e6502707d86cc8914e8d81f7e66db73f4b4a2ef8-4952143-images-thumbs&n=13" alt="logo" />
+      </header>
+      <nav className='nav'>
+        nav
+      </nav>
+      <div className='content'>
+        <img src="https://www.borealcm.com/wp-content/uploads/2015/06/placeholderimg-1024x576.png" alt="pfp" />
+        <div>ava + desc</div>
+        <div>
+          My posts
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post1
+            </div>
+            <div>
+              post2
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
