59946bb44102dedca5de86c56edae7278f50bcb3
TMWAG <semyon.valyaev@gmail.com>
2023-05-12 14:44:48 +0300

    ссылки обёрнуты в дивы, стилизация

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,21 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <div><a href="#">Profile</a></div>
-      <div><a href="#">Messages</a></div>
-      <div><a href="#">News</a></div>
-      <div><a href="#">Music</a></div>
-      <div><a href="#">Settings</a></div>
+      <div className={styles.item}>
+        <a href="#">Profile</a>
+      </div>
+      <div className={styles.item}>
+        <a href="#">Messages</a>
+      </div>
+      <div className={styles.item}>
+        <a href="#">News</a>
+      </div>
+      <div className={styles.item}>
+        <a href="#">Music</a>
+      </div>
+      <div className={styles.item}>
+        <a href="#">Settings</a>
+      </div>
     </nav>
   );
 };
