1c6d4a9ad4ac35461493641b5834266ec99e5224
TMWAG <semyon.valyaev@gmail.com>
2023-05-13 11:18:42 +0300

    добавлен список диалогов и сообщений

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,11 +1,53 @@
 import styles from './Messages.module.css';
 
-const Dialogs = () => {
+const Messages = () => {
   return (
-    <div>
-      Messages
+    <div className={styles.dialogsBox}>
+      <div className={styles.dialogsItems}>
+        <div className={styles.dialog}>
+          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Dima" />
+          Dima
+        </div>
+        <div className={styles.dialog}>
+          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Andrew" />
+          Andrew
+        </div>
+        <div className={styles.dialog}>
+          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Sasha" />
+          Sasha
+        </div>
+        <div className={styles.dialog}>
+          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Ira" />
+          Ira
+        </div>
+        <div className={styles.dialog}>
+          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Victor" />
+          Victor
+        </div>
+        <div className={styles.dialog + ' ' + styles.active}>
+          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Slava" />
+          Slava
+        </div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>
+          Hello
+        </div>
+        <div className={styles.message}>
+          I
+        </div>
+        <div className={styles.message}>
+          Am
+        </div>
+        <div className={styles.message}>
+          gnome
+        </div>
+        <div className={styles.message}>
+          and you been gnomed
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Dialogs;
+export default Messages;
