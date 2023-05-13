d6fdbbb9f3b744bd5cb9931aa26b9af42d73f64e
TMWAG <semyon.valyaev@gmail.com>
2023-05-13 12:34:24 +0300

    добавлены NavLink для каждого диалога, стилизация

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,33 +1,47 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Messages.module.css';
 
 const Messages = () => {
   return (
     <div className={styles.dialogsBox}>
       <div className={styles.dialogsItems}>
-        <div className={styles.dialog}>
-          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Dima" />
-          Dima
-        </div>
-        <div className={styles.dialog}>
-          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Andrew" />
-          Andrew
-        </div>
-        <div className={styles.dialog}>
-          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Sasha" />
-          Sasha
-        </div>
-        <div className={styles.dialog}>
-          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Ira" />
-          Ira
-        </div>
-        <div className={styles.dialog}>
-          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Victor" />
-          Victor
-        </div>
-        <div className={styles.dialog + ' ' + styles.active}>
-          <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Slava" />
-          Slava
-        </div>
+        <NavLink to='/dialogs/1'>
+          <div className={styles.dialog}>
+            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Dima" className={styles.dialogPicture} />
+            Dima
+          </div>
+        </NavLink>
+        <NavLink to='/dialogs/2'>
+          <div className={styles.dialog}>
+            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Andrew" className={styles.dialogPicture} />
+            Andrew
+          </div>
+        </NavLink>
+        <NavLink to='/dialogs/3'>
+          <div className={styles.dialog}>
+            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Sasha" className={styles.dialogPicture} />
+            Sasha
+          </div>
+        </NavLink>
+        <NavLink to='/dialogs/4'>
+          <div className={styles.dialog}>
+            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Ira" className={styles.dialogPicture} />
+            Ira
+          </div>
+        </NavLink>
+        <NavLink to='/dialogs/5'>
+          <div className={styles.dialog}>
+            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Victor" className={styles.dialogPicture} />
+            Victor
+          </div>
+        </NavLink>
+        <NavLink to='/dialogs/'>
+          <div className={styles.dialog + ' ' + styles.active}>
+            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Slava" className={styles.dialogPicture} />
+            Slava
+          </div>
+        </NavLink>
+
       </div>
       <div className={styles.messages}>
         <div className={styles.message}>
