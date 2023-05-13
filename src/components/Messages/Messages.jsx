068c0db04617e7a05e73cbace06a899707f01c47
TMWAG <semyon.valyaev@gmail.com>
2023-05-13 23:02:25 +0300

    разбиение на компоненты

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,64 +1,66 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Messages.module.css';
 
+const DialogItem = (props) => {
+  return (
+    <NavLink to={`/messages/${props.userId}`}>
+      <div className={styles.dialog}>
+        <img src={props.profilePicture} alt={props.userName} className={styles.dialogPicture} />
+        {props.userName}
+      </div>
+    </NavLink>
+  );
+}; 
+
+const MessageItem = (props) => {
+  return (
+    <div className={styles.message}>
+      {props.message}
+    </div>
+  );
+};
+
 const Messages = () => {
   return (
     <div className={styles.dialogsBox}>
       <div className={styles.dialogsItems}>
-        <NavLink to='/dialogs/1'>
-          <div className={styles.dialog}>
-            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Dima" className={styles.dialogPicture} />
-            Dima
-          </div>
-        </NavLink>
-        <NavLink to='/dialogs/2'>
-          <div className={styles.dialog}>
-            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Andrew" className={styles.dialogPicture} />
-            Andrew
-          </div>
-        </NavLink>
-        <NavLink to='/dialogs/3'>
-          <div className={styles.dialog}>
-            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Sasha" className={styles.dialogPicture} />
-            Sasha
-          </div>
-        </NavLink>
-        <NavLink to='/dialogs/4'>
-          <div className={styles.dialog}>
-            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Ira" className={styles.dialogPicture} />
-            Ira
-          </div>
-        </NavLink>
-        <NavLink to='/dialogs/5'>
-          <div className={styles.dialog}>
-            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Victor" className={styles.dialogPicture} />
-            Victor
-          </div>
-        </NavLink>
-        <NavLink to='/dialogs/'>
-          <div className={styles.dialog + ' ' + styles.active}>
-            <img src="https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg" alt="Slava" className={styles.dialogPicture} />
-            Slava
-          </div>
-        </NavLink>
-
+        <DialogItem
+          userId='1' 
+          profilePicture='https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg' 
+          userName='Dima' 
+        />
+        <DialogItem
+          userId='2'
+          profilePicture='https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg'
+          userName='Andrew'
+        />
+        <DialogItem
+          userId='3'
+          profilePicture='https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg'
+          userName='Sasha'
+        />
+        <DialogItem
+          userId='4'
+          profilePicture='https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg'
+          userName='Ira'
+        />
+        <DialogItem
+          userId='5'
+          profilePicture='https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg'
+          userName='Victor'
+        />
+        <DialogItem
+          userId='6'
+          profilePicture='https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg'
+          userName='Slava'
+        />
       </div>
       <div className={styles.messages}>
-        <div className={styles.message}>
-          Hello
-        </div>
-        <div className={styles.message}>
-          I
-        </div>
-        <div className={styles.message}>
-          Am
-        </div>
-        <div className={styles.message}>
-          gnome
-        </div>
-        <div className={styles.message}>
-          and you been gnomed
-        </div>
+        <MessageItem message='Hello' />
+        <MessageItem message='I' />
+        <MessageItem message='Am' />
+        <MessageItem message='gnome' />
+        <MessageItem message='and you been gnomed' />
       </div>
     </div>
   );
