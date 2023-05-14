dc789c8e7c4d9c002c16a3022d074b55b3dcef2e
TMWAG <semyon.valyaev@gmail.com>
2023-05-14 14:06:22 +0300

    вынесение данных на уровень index.js

--- src/App.js
+++ src/App.js
@@ -8,30 +8,7 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
-const App = () => {
-  let dialogsData = [
-    { userId: 1, userName: 'Dima', profilePicture: 'https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg' },
-    { userId: 2, userName: 'Anderew', profilePicture: 'https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg' },
-    { userId: 3, userName: 'Sasha', profilePicture: 'https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg' },
-    { userId: 4, userName: 'Ira', profilePicture: 'https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg' },
-    { userId: 5, userName: 'Victor', profilePicture: 'https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg' },
-    { userId: 6, userName: 'Slava', profilePicture: 'https://mulinecxc.ru/wp-content/uploads/2020/08/106400558.jpg' },
-  ];
-
-  let messagesData = [
-    { id: 1, message: 'Hello' },
-    { id: 2, message: 'I' },
-    { id: 3, message: 'Am' },
-    { id: 4, message: 'Gnome' },
-    { id: 5, message: 'And you been gnoomed' },
-  ];
-
-  let postsData = [
-    { id: 1, message: 'Hello world', likes: 12},
-    { id: 1, message: 'Eeeeeee', likes: 2},
-    { id: 1, message: 'Greg', likes: 9000},
-  ];
-
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className='app-wrapper'>
@@ -39,11 +16,31 @@ const App = () => {
         <Navbar />
         <div className='app-content-wrapper'>
           <Routes>
-            <Route path='/profile' element={<Profile postsData={postsData}/>} />
-            <Route path='/messages/*' element={<Messages dialogsData={dialogsData} messagesData={messagesData} />} />
-            <Route path='/news' element={<News />} />
-            <Route path='/music' element={<Music />} />
-            <Route path='/settings' element={<Settings />} />
+            <Route
+              path='/profile' 
+              element={<Profile 
+                postsData={props.postsData}
+              />} 
+            />
+            <Route 
+              path='/messages/*' 
+              element={<Messages 
+              dialogsData={props.dialogsData} 
+              messagesData={props.messagesData} 
+              />} 
+            />
+            <Route
+              path='/news'
+              element={<News />}
+            />
+            <Route 
+              path='/music' 
+              element={<Music />} 
+            />
+            <Route 
+              path='/settings' 
+              element={<Settings />} 
+            />
           </Routes>
         </div>
       </div>
