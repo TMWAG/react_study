03e44e7162998259dc1f637e3304429855d0a3da
TMWAG <semyon.valyaev@gmail.com>
2023-05-17 16:06:06 +0300

    убрана перерисовка всей станицы на каждое изменение

--- src/index.js
+++ src/index.js
@@ -7,26 +7,16 @@ import store from './redux/redux-store';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-const rerenderEntireTree = (state) => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-};
-
-rerenderEntireTree(store.getState());
-store.subscribe(() => {
-  const state = store.getState();
-  rerenderEntireTree(state);
-});
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+          <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
