08e7e06f53c4393eca829cbb900d3736557ca35d
TMWAG <semyon.valyaev@gmail.com>
2023-05-20 19:42:49 +0300

    изменён тип с функционально на классовую

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,20 +3,33 @@ import UserItem from './UserItem/UserItem';
 import styles from './Users.module.css';
 import axios from 'axios';
 
-const Users = (props) => {  
-  if (!props.users.length) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
     axios.get('https://social-network.samuraijs.com/api/1.0/users')
       .then(response => {
         props.setUsers(response.data.items);
       })
-    
-  };
-  const userElements = props.users.map(u => <UserItem key={u.id} user={u} follow={props.follow} unfollow={props.unfollow} />)
-  return (
-    <div className={styles.users}>
-      {userElements}
-    </div>
-  );
-};
+  }
+  render() {
+    const usersElements = this.props.users.map((u) => 
+      {
+        return (
+          <UserItem
+            key={u.id} 
+            user={u} 
+            follow={this.props.follow} 
+            unfollow={this.props.unfollow}
+          />
+        );
+      }
+    )
+    return(
+      <div className={styles.users}>
+        {usersElements}
+      </div>
+    );
+  }
+}
 
 export default Users;
